35985bc70aef992550fa7934b54ffa1e0877670b
trinhnta137 <nguyen.thi.anh.trinh@sun-asterisk.com>
2020-08-22 17:12:06 +0700

    Create API data testing driven demo

---
+++ Object Repository/get user.rs
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>get user</name>
+   <tag></tag>
+   <elementGuidId>8869dbac-f441-46ef-9cae-9428fa25dde8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>7.5.5</katalonVersion>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://reqres.in/api/users/3</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <variables>
+      <defaultValue>''</defaultValue>
+      <description></description>
+      <id>3e99e05d-16a2-4258-a89e-c243dcf1911d</id>
+      <masked>false</masked>
+      <name>variable</name>
+   </variables>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ Test Suites/testing demo.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testing demo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e4ae4933-5371-4686-8412-aeed5e9fe881</testSuiteGuid>
+   <testCaseLink>
+      <guid>683d5608-0cc8-413a-8e8a-836e66022bfa</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API Data Driven Testing</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>a9ff2740-bce7-4c54-8d93-f1b2127c1418</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/User_Test</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>a9ff2740-bce7-4c54-8d93-f1b2127c1418</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Full Name</value>
+         <variableId>5568b24f-2168-441d-9dc1-5600e5556bae</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>a9ff2740-bce7-4c54-8d93-f1b2127c1418</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Job</value>
+         <variableId>181218ac-91b4-43ad-8cc8-7d2c986850ad</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
